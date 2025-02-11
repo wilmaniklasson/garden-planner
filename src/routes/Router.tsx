43809c118571206158a94b000ab558dd8d997234
wilmaniklasson <wilma.niklasson2@hotmail.se>
2025-02-11 14:02:55 +0100

    använder Suspense och React.lazy för att dynamisk ladda komponenterna

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,17 +1,23 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import HomePage from "../pages/HomePage/HomePage";
-import CanvasPage from "../pages/CanvasPage/CanvasPage";
+import { Suspense } from "react";
+import React from "react";
+
+// Dynamiska importeringar
+const LoginPage = React.lazy(() => import("../pages/LoginPage/LoginPage"));
+const HomePage = React.lazy(() => import("../pages/HomePage/HomePage"));
+const CanvasPage = React.lazy(() => import("../pages/CanvasPage/CanvasPage"));
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/canvas" element={<CanvasPage />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/canvas" element={<CanvasPage />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 };
 
