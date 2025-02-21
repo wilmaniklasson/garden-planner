30f02b929203cbe1c6a648be06bd91bd1d24416c
wilmaniklasson <wilma.niklasson2@hotmail.se>
2025-02-21 17:59:21 +0100

    -

--- src/components/SidePanel/Decoration.tsx
+++ src/components/SidePanel/Decoration.tsx
@@ -2,13 +2,23 @@ import { MdFence } from 'react-icons/md';
 import './Options.css';
 import OptionProps from './OptionProps';
 import Pool from '../../assets/images/Decoration/Pool.svg';
-import Stone from '../../assets/images/Decoration/Stone.svg';
-import OptionsList from './OptionsList';  // Importera den nya komponenten
+import OptionsList from './OptionsList';
+import StonePatio from '../../assets/images/Decoration/Stone-Patio.svg';
+import Pathway from '../../assets/images/Decoration/Pathway.svg';
+import PathwayCorner from '../../assets/images/Decoration/Pathway-corner.svg';
+import House from '../../assets/images/Decoration/House3.svg';
+import StonePatioHexagon from '../../assets/images/Decoration/Stone-Patio-Hexagon.svg';
+import PearShapedPool from '../../assets/images/Decoration/pear-shaped-pool.svg';
 
 const Decoration: React.FC<OptionProps> = ({ selectedTool, handleSelectTool, isOpen, toggle }) => {
   const items = [
-    { name: 'Pool', img: Pool },
-    { name: 'Stone', img: Stone }
+    { name: 'Pear Shaped Pool', img: PearShapedPool },
+    { name: 'Stone Patio', img: StonePatio },
+    { name: 'Stone Patio Hexagon', img: StonePatioHexagon },
+    { name: 'Pathway', img: Pathway },
+    { name: 'Pathway Corner', img: PathwayCorner },
+    { name: 'House', img: House }
+
   ];
 
   return (
