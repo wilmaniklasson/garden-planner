4057c5b9ac95ca7e289e58e45c8008203d31f370
wilmaniklasson <wilma.niklasson2@hotmail.se>
2025-02-22 14:46:40 +0100

    svg

--- src/components/SidePanel/Decoration.tsx
+++ src/components/SidePanel/Decoration.tsx
@@ -20,6 +20,13 @@ import WoodenFence from '../../assets/images/Decoration/Wooden-Fence.svg';
 import DiningSet from '../../assets/images/Decoration/Dining-Set.svg';
 import WoodenBench from '../../assets/images/Decoration/Wooden-Bench.svg';
 import WoodenTable from '../../assets/images/Decoration/Wooden-Table.svg';
+import StoneNr1 from '../../assets/images/Decoration/Stone-Nr-1.svg';
+import StoneNr2 from '../../assets/images/Decoration/Stone-Nr-2.svg';
+import StoneNr3 from '../../assets/images/Decoration/Stone-Nr-3.svg';
+import StoneNr4 from '../../assets/images/Decoration/Stone-Nr-4.svg';
+import StoneNr5 from '../../assets/images/Decoration/Stone-Nr-5.svg';
+import StoneNr6 from '../../assets/images/Decoration/Stone-Nr-6.svg';
+import StonePatioTerracotta from '../../assets/images/Decoration/Stone-Patio-Terracotta.svg';
 const Decoration: React.FC<OptionProps> = ({ selectedTool, handleSelectTool, isOpen, toggle }) => {
   const items = [
     { name: 'Stone Patio', img: StonePatio },
@@ -39,7 +46,14 @@ const Decoration: React.FC<OptionProps> = ({ selectedTool, handleSelectTool, isO
     { name: 'Wooden Fence', img: WoodenFence },
     { name: 'Dining Set', img: DiningSet },
     { name: 'Wooden Bench', img: WoodenBench },
-    { name: 'Wooden Table', img: WoodenTable }
+    { name: 'Wooden Table', img: WoodenTable },
+    { name: 'Stone Nr. 1', img: StoneNr1 },
+    { name: 'Stone Nr. 2', img: StoneNr2 },
+    { name: 'Stone Nr. 3', img: StoneNr3 },
+    { name: 'Stone Nr. 4', img: StoneNr4 },
+    { name: 'Stone Nr. 5', img: StoneNr5 },
+    { name: 'Stone Nr. 6', img: StoneNr6 },
+    { name: 'Stone Patio Terracotta', img: StonePatioTerracotta},
 
 
     
