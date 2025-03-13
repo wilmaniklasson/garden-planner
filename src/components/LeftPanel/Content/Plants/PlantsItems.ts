705423e6c2585046f91c02b42bed9703bd79063d
wilmaniklasson <wilma.niklasson2@hotmail.se>
2025-03-13 18:35:03 +0100

    -

--- src/components/LeftPanel/Content/Plants/PlantsItems.ts
+++ src/components/LeftPanel/Content/Plants/PlantsItems.ts
@@ -1,42 +1,23 @@
-
-import PlantNr1 from '../../../../assets/images/Plants/Plant-Nr-1.svg';
-import PlantNr2 from '../../../../assets/images/Plants/Plant-Nr-2.svg';
-import PlantNr3 from '../../../../assets/images/Plants/Plant-Nr-3.svg';
-import PlantNr4 from '../../../../assets/images/Plants/Plant-Nr-4.svg';
-import PlantNr5 from '../../../../assets/images/Plants/Plant-Nr-5.svg';
-import PlantNr6 from '../../../../assets/images/Plants/Plant-Nr-6.svg'; 
-import PlantNr7 from '../../../../assets/images/Plants/Plant-Nr-7.svg';
-import PlantNr8 from '../../../../assets/images/Plants/Plant-Nr-8.svg';
-import PlantNr9 from '../../../../assets/images/Plants/Plant-Nr-9.svg';
-import PlantNr10 from '../../../../assets/images/Plants/Plant-Nr-10.svg';
-import PlantNr11 from '../../../../assets/images/Plants/Plant-Nr-11.svg';
-import PlantNr12 from '../../../../assets/images/Plants/Plant-Nr-12.svg';
-import PlantNr13 from '../../../../assets/images/Plants/Plant-Nr-13.svg';
-import PlantNr14 from '../../../../assets/images/Plants/Plant-Nr-14.svg';
-import PlantNr15 from '../../../../assets/images/Plants/Plant-Nr-15.svg';
-import PlantNr16 from '../../../../assets/images/Plants/Plant-Nr-16.svg';
-
 const items: {
     name: string;
     img: string;
 }[] = [
-    { name: 'Plant Nr 1', img: PlantNr1 },
-    { name: 'Plant Nr 2', img: PlantNr2 },
-    { name: 'Plant Nr 3', img: PlantNr3 },
-    { name: 'Plant Nr 4', img: PlantNr4 },
-    { name: 'Plant Nr 5', img: PlantNr5 },
-    { name: 'Plant Nr 6', img: PlantNr6 },
-    { name: 'Plant Nr 7', img: PlantNr7 },
-    { name: 'Plant Nr 8', img: PlantNr8 },
-    { name: 'Plant Nr 9', img: PlantNr9 },
-    { name: 'Plant Nr 10', img: PlantNr10 },
-    { name: 'Plant Nr 11', img: PlantNr11 },
-    { name: 'Plant Nr 12', img: PlantNr12 },
-    { name: 'Plant Nr 13', img: PlantNr13 },
-    { name: 'Plant Nr 14', img: PlantNr14 },
-    { name: 'Plant Nr 15', img: PlantNr15 },
-    { name: 'Plant Nr 16', img: PlantNr16 },
-  
-  ];
+    { name: 'Plant Nr 1', img: '/images/Plants/Plant-Nr-1.svg' },
+    { name: 'Plant Nr 2', img: '/images/Plants/Plant-Nr-2.svg' },
+    { name: 'Plant Nr 3', img: '/images/Plants/Plant-Nr-3.svg' },
+    { name: 'Plant Nr 4', img: '/images/Plants/Plant-Nr-4.svg' },
+    { name: 'Plant Nr 5', img: '/images/Plants/Plant-Nr-5.svg' },
+    { name: 'Plant Nr 6', img: '/images/Plants/Plant-Nr-6.svg' },
+    { name: 'Plant Nr 7', img: '/images/Plants/Plant-Nr-7.svg' },
+    { name: 'Plant Nr 8', img: '/images/Plants/Plant-Nr-8.svg' },
+    { name: 'Plant Nr 9', img: '/images/Plants/Plant-Nr-9.svg' },
+    { name: 'Plant Nr 10', img: '/images/Plants/Plant-Nr-10.svg' },
+    { name: 'Plant Nr 11', img: '/images/Plants/Plant-Nr-11.svg' },
+    { name: 'Plant Nr 12', img: '/images/Plants/Plant-Nr-12.svg' },
+    { name: 'Plant Nr 13', img: '/images/Plants/Plant-Nr-13.svg' },
+    { name: 'Plant Nr 14', img: '/images/Plants/Plant-Nr-14.svg' },
+    { name: 'Plant Nr 15', img: '/images/Plants/Plant-Nr-15.svg' },
+    { name: 'Plant Nr 16', img: '/images/Plants/Plant-Nr-16.svg' },
+];
 
-  export default items;
+export default items;
