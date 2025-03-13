1205e2a50782f85c49f2bf26831743c312c3fd3f
wilmaniklasson <wilma.niklasson2@hotmail.se>
2025-03-13 21:28:24 +0100

    -

--- src/components/LeftPanel/Content/GroundMaterials/GroundMaterialsItems.ts
+++ src/components/LeftPanel/Content/GroundMaterials/GroundMaterialsItems.ts
@@ -1,22 +1,14 @@
-import circleGrass from '../../../../assets/images/GroundMaterials/circle-grass.svg';
-import rectGrass from '../../../../assets/images/GroundMaterials/rect-grass.svg';
-import gardenBed from '../../../../assets/images/GroundMaterials/garden-bed.svg';
-import circleGardenBed from '../../../../assets/images/GroundMaterials/circle-garden-bed.svg';
-import WedgeGardenBed from '../../../../assets/images/GroundMaterials/wedge-garden-bed.svg';
-import WedgeGrass from '../../../../assets/images/GroundMaterials/wedge-grass.svg';
-import WedgeGardenBed90Angle from '../../../../assets/images/GroundMaterials/Wedge-Garden-Bed-90-angel.svg';
-
 const items: {
     name: string;
     img: string;
 }[] = [
-    { name: 'Rect Lawn', img: rectGrass },
-    {  name: 'Round Lawn', img: circleGrass },
-    { name: 'Wedge Grass', img: WedgeGrass },
-    { name: 'Garden Bed', img: gardenBed },
-    { name: 'Round Garden Bed', img: circleGardenBed },
-    { name: 'Wedge Garden Bed', img: WedgeGardenBed },
-    { name: 'Wedge Garden Bed', img: WedgeGardenBed90Angle }
+    { name: 'Rect Lawn', img: '../../../../assets/images/GroundMaterials/rect-grass.svg' },
+    { name: 'Round Lawn', img: '../../../../assets/images/GroundMaterials/circle-grass.svg' },
+    { name: 'Wedge Grass', img: '../../../../assets/images/GroundMaterials/wedge-grass.svg' },
+    { name: 'Garden Bed', img: '../../../../assets/images/GroundMaterials/garden-bed.svg' },
+    { name: 'Round Garden Bed', img: '../../../../assets/images/GroundMaterials/circle-garden-bed.svg' },
+    { name: 'Wedge Garden Bed', img: '../../../../assets/images/GroundMaterials/wedge-garden-bed.svg' },
+    { name: 'Wedge Garden Bed', img: '../../../../assets/images/GroundMaterials/Wedge-Garden-Bed-90-angel.svg' }
 ];
 
 export default items;
